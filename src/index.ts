6af06b10af53d71c027347b4031efc7f3d45e233
Dan Turner <dan.turner.djt@gmal.com>
2023-08-01 00:08:16 +0100

    export required consumer types in index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { VerbInfo } from "./Defs/VerbDefs";
 import { FormInfo } from "./Defs/VerbFormDefs";
 import { Result, processAndGetConjugation } from "./Process/Process";
 
+export { VerbInfo } from "./Defs/VerbDefs";
+export { FormInfo } from "./Defs/VerbFormDefs";
+export { Result } from "./Process/Process";
+
 export function getVerbConjugation(verbInfo: VerbInfo, formInfo: FormInfo): Result | Error {
   return processAndGetConjugation(verbInfo, formInfo);
 }
